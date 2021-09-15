32a34b40e0899d72dcf8cff6c7371497a13b254e
Your Name <tonthat90@gmail.com>
2021-09-15 15:54:34 +0700

    proccess with DB

--- testSql.sh
+++ testSql.sh
@@ -5,8 +5,9 @@ DB_Password="123456"
 DB_Hostname="localhost"
 DB_Port="1521"
 DB_SID="ORCL"
+DB_Connect="sqlplus -S ${DB_UserName}/${DB_Password}@//${DB_Hostname}:${DB_Port}/${DB_SID}"
 
-sqlplus -S ${DB_UserName}/${DB_Password}@//${DB_Hostname}:${DB_Port}/${DB_SID} <<EOF >database_status_$current_date.log
+${DB_Connect} <<EOF >show_data_$current_date.log
 set lines 12345 pages 12345;
 
 col username for a30;
@@ -17,3 +18,38 @@ select * from customers;
 --insert into customers values ('3', 'cc', 'cc');
 EXIT
 EOF
+
+# get data from ABC.customers
+list=`${DB_Connect} <<EOF
+set feedback off
+set serveroutput on
+set heading off
+set pagesize 0
+set tab off
+select customer_name from customers FETCH NEXT 5 ROWS ONLY
+/
+exit
+EOF`
+
+# Insert data from ABC.customers to ABCDEF.Orders
+function insert_datas() {
+    ${DB_Connect} <<EOF >insert_data_$current_date_$1.log
+    set lines 12345 pages 12345;
+
+    col username for a30;
+    col open_mode for a30;
+    insert into ABCDEF.ORDERS VALUES ('$1', '$2', 'a');
+    EXIT
+EOF
+}
+
+i=0
+for value in ${list}
+do
+    customer_name[$i]=${value}
+    id=$i
+    customer_name=${customer_name[$i]}
+    echo "customer_name[$i] = ${customer_name[$i]}"
+    insert_datas $id $customer_name
+    let i+=1
+done
